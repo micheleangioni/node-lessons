6aeb6320890fa8f1f0fd09df083b69fa02890387
Michele <angioni@oak-labs.com>
2018-02-21 18:26:51 +0100

    Added dynamical query parameters

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require('express')
-const app = express()
+const app = express();
 const https = require('https');
 const fs = require('fs');
 
+/**
+ * This function could be heavily simplified or avoided by using the 'request' or 'axios' Node modules.
+ *
+ * @param {string} baseUrl
+ * @param {array} query
+ * @param {array} queryNames
+ */
+const addQueryParameters = (baseUrl, query, queryNames) => {
+  const values = queryNames.reduce((acc, queryName) => {
+    if (query[queryName]) {
+      acc[queryName] = query[queryName];
+    }
+
+    return acc;
+  }, {});
+
+  let first = true;
+
+  for (const queryName in values) {
+    const queryValue = values[queryName];
+    baseUrl += first === true ? '?' : '&';
+    baseUrl += `${queryName}=${queryValue}`;
+
+    first = false;
+  }
+
+  return baseUrl
+};
+
 const retrieveJobs = (req, res) => {
-  https.get('https://jobs.github.com/positions.json?description=python&location=sf&full_time=true', (response) => {
+  let baseUrl = 'https://jobs.github.com/positions.json'
+  let fullUrl = addQueryParameters(baseUrl, req.query, ['location', 'full_time'])
+
+  https.get(fullUrl, (response) => {
     let data = '' ;
 
     response.on('data', (chunk) => {
