a0d9280bdc0ce17e075b4793ddace497acd82917
Michele <angioni@oak-labs.com>
2018-02-21 17:56:05 +0100

    Fetching jobs from Github

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const app = express()
+const https = require('https');
+const fs = require('fs');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const retrieveJobs = (req, res) => {
+  https.get('https://jobs.github.com/positions.json?description=python&location=sf&full_time=true', (response) => {
+    let data = '' ;
+
+    response.on('data' , function (chunk) {
+      data += chunk.toString();
+    });
+
+    response.on('end' ,  function() {
+      res.json(data)
+    });
+  }).on('error', (e) => {
+    console.error('ERROR');
+    console.error(e);
+  });
+}
+
+app.get('/', retrieveJobs);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
